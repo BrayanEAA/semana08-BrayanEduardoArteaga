2227a0d4386571b4c1e3e8257f0d507700455d31
BEA <124007083+BrayanEAA@users.noreply.github.com>
2023-03-15 23:47:57 -0600

    Update Program.cs

--- semana8/pro8/Program.cs
+++ semana8/pro8/Program.cs
@@ -11,7 +11,7 @@ decimal precio = decimal.Parse(Console.ReadLine());
 Console.WriteLine("Ingrese el stock del producto: ");
 int stock = int.Parse(Console.ReadLine());
 
-// Insertar el producto en la base de datos
+
 var producto = new Producto { Nombre = nombre, Descripcion = descripcion, Precio = precio, Stock = stock };
 db.Productos.Add(producto);
 db.SaveChanges();
